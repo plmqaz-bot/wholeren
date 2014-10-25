6af350669300cc8cd594315b4e8fb59968f8c712
Han <han@ubuntu.(none)>
2014-10-25 00:16:33 -0400

    changed to Service.js

---
+++ api/models/Service.js
@@ -0,0 +1,24 @@
+/**
+* Service.js
+*
+* @description :: TODO: You might write a short summary of how this model works and what it represents here.
+* @docs        :: http://sailsjs.org/#!documentation/models
+*/
+
+module.exports = {
+
+  attributes: {  	
+  	contract:{model:'Contract'},
+
+  	serviceType:{model:'ServiceType'},
+
+  	progress:{type:'int'},
+
+    serviceTeacher:{model:'User'},
+
+    application:{collection:'Application', via:'service'},
+
+    comment:{collection:'Comment',via:'service'}
+  }
+};
+
