f002cf3d00602fddcdf1800a6141b2b8c4f08741
Han Lai <han.lai321@gmail.com>
2015-01-12 22:57:07 -0500

    a

---
+++ api/models/ServiceProgress.js
@@ -0,0 +1,14 @@
+/**
+* ServiceProgress.js
+*
+* @description :: TODO: You might write a short summary of how this model works and what it represents here.
+* @docs        :: http://sailsjs.org/#!documentation/models
+*/
+
+module.exports = {
+
+  attributes: {
+  	serviceProgress:{type:'string',required:true,unique:true}
+  }
+};
+
