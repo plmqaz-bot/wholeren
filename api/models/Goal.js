e259b32d703a85859508b31ba4406c53f4ce9557
Han Lai <han.lai321@gmail.com>
2015-01-23 01:38:05 -0500

    goal.js

---
+++ api/models/Goal.js
@@ -0,0 +1,22 @@
+/**
+* Goal.js
+*
+* @description :: TODO: You might write a short summary of how this model works and what it represents here.
+* @docs        :: http://sailsjs.org/#!documentation/models
+*/
+
+module.exports = {
+
+  attributes: {
+  	user:{model:'User'},
+  	month:{type:'integer'},
+  	year:{type:'integer'},
+  	transferSaleGoal:{type:'integer'},
+  	transferExpGoal:{type:'integer'},
+  	emergSaleGoal:{type:'integer'},
+  	emergExpGoal:{type:'integer'},
+  	leadGoal:{type:'integer'},
+
+  }
+};
+
