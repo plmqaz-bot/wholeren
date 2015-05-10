5cba4c639be0aa7e5c192eb2ac2edb02aa812795
nickrechard <han.lai321@gmail.com>
2015-05-10 02:20:43 -0400

    comission setup table is ready

--- Client/views/settings.js
+++ Client/views/settings.js
@@ -363,7 +363,7 @@ Settings.hierarchy=main.baseDataView.extend({
         // var popUpView = new LookupForm({collection:this.collection});
         // popUpView.render()
         // $('.app').html(popUpView.el);
-        var toAdd=new Obiwang.Models.syncModel(null,{_url:'/WhoOwnsWho/'});
+        var toAdd=new Obiwang.Models.syncModel(null,{_url:collectionUrl});
         var self=this;
         toAdd.save(null,{
             success:function(model){
@@ -377,11 +377,55 @@ Settings.hierarchy=main.baseDataView.extend({
     },
 });
 
+Settings.comissionLookup=Settings.hierarchy.extend({
+    collectionUrl:'/ComissionLookup/',
+    title:'销售佣金设定',
+   // filterFields:['puppet','boss'],
+    constructColumns:function(){
+        var self=this;
+        var editable=false;
+        if(parseInt(this.rank||"1")==3){
+            editable=true;
+        }
+        return util.ajaxGET('/contract/getAllOptions/').then(function(AllOptions){
+            var t=_.map(AllOptions['SalesGroup'],function(e){return [e.salesGroup,e.id]});
+            t.push(['任何销售组',0]);
+            var salesgroup=BackgridCells.SelectCell({name:"SalesGroup",values:t});
+            t=_.map(AllOptions['Lead'],function(e){return [e.lead,e.id]});
+            t.push(['Any Lead',0]);
+            var lead=BackgridCells.SelectCell({name:"Lead",values:t});
+            var leadDetail=lead.extend({
+                optionValues:function(){
+                    var l=this.model.get('lead')||0;
+                    var shrunk=_.where(AllOptions['LeadDetail'],{lead:l});
+                    var toadd=_.map(shrunk,function(e){return [e.leadDetail,e.id]});
+                    toadd.push(['Any LeadDetail',0]);
+                    return [{name:'LeadDetail',values:toadd}];
+                }
+            });
+            
+            self.columns=[
+                {name:'lead',label:'Lead种类',editable:editable,cell:lead},
+                {name:'leadDetail',label:'lead种类细分',editable:editable,cell:leadDetail},
+                {name:'salesGroup',label:'销售组',editable:editable,cell:salesgroup},
+                {name:'rolename',label:'销售角色',editable:editable,cell:'string'},
+                {name:'comission',label:'佣金百分比',editable:editable,cell:Backgrid.NumberCell.extend({decimals:3})},
+            ];
+            // self.selectFields=[
+            // {name:'puppet',options:_.map(user,function(e){return [e.nickname,e.id]})},
+            // {name:'boss',options:_.map(user,function(e){return [e.nickname,e.id]})},
+            // ];
+            return Promise.resolve({});
+        });
+    },
+
+});
 var MenuTitle={
     user:'个人资料',
     allUsers:'UserControl',
     lookup:'Comission机制',
-    hierarchy:'老师等级机制'
+    hierarchy:'老师等级机制',
+    comissionLookup:'销售佣金设定'
 }
 
 
