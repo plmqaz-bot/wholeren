1370e082834da46e5216fb8f110f568779c46c9e
nickrechard <han.lai321@gmail.com>
2015-05-28 03:34:06 -0400

    removed the field which cause service non-unique

--- api/controllers/ServiceController.js
+++ api/controllers/ServiceController.js
@@ -5,7 +5,7 @@
  * @help        :: See http://links.sailsjs.org/docs/controllers
  */
  function constructsql(where,who){
-			return "select distinct cli.chineseName as 'matchedcName',client.chineseName,u.nickname,service.*,c.contractSigned, servicetype.serviceType as 'type',servicetype.addApplication, c.gpa,c.toefl,c.sat,c.gre,c.otherScore, c.degree, c.targetSchoolDegree , c.major, c.previousSchool,c.endFee, servrole.servRole \
+			return "select distinct cli.chineseName as 'matchedcName',client.chineseName,u.nickname,service.*,c.contractSigned, servicetype.serviceType as 'type',servicetype.addApplication, c.gpa,c.toefl,c.sat,c.gre,c.otherScore, c.degree, c.targetSchoolDegree , c.major, c.previousSchool,c.endFee \
 			from service \
 			left join contract c on c.id=service.contract \
 			left join client on c.client=client.id \
