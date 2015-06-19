667fb7a48ee7c46d3bf460b12332a149becb2c64
Han Lai <han.lai321@gmail.com>
2015-06-19 01:08:31 -0400

    added the relationship between subrole and salesgroup

---
+++ api/models/SubRole_handle_salesGroup.js
@@ -0,0 +1,15 @@
+/**
+* SubRole_handle_salesGroup.js
+*
+* @description :: TODO: You might write a short summary of how this model works and what it represents here.
+* @docs        :: http://sailsjs.org/#!documentation/models
+*/
+
+module.exports = {
+
+  attributes: {
+  	subRole:{model:'SubRole'},
+  	salesGroup:{model:'SalesGroup'}
+  }
+};
+
