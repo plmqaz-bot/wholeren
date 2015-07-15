50ab04b44f95c30e66c6ca2cd86f3c80aa947e8f
Han Lai <han.lai321@gmail.com>
2015-07-15 18:07:41 -0400

    import GPA

--- importGPA.py
+++ importGPA.py
@@ -7,7 +7,7 @@ import re;
 import time;
 
 
-cnx=mysql.connector.connect(user='wholeren',password='3000201S',host='50.163.243.196',database='wholeren',charset='utf8');
+cnx=mysql.connector.connect(user='wholeren',password='3000201S',host='localhost',database='wholeren',charset='utf8');
 cursor=cnx.cursor();
 
 def toInt(s):
@@ -40,15 +40,15 @@ with open('gpa.csv','rb') as csvfile:
 		sat=toFloat(line[5]);
 		gre=toFloat(line[6]);
 		gmat=toFloat(line[7]);
-		print "execute "+name;
-		# cursor.execute("SELECT distinct contract.id from servicedetail inner join contract on contract.id=servicedetail.contract where servicedetail.cName=\'"+name+"\'");
+		#print "execute "+name;
+		cursor.execute("SELECT distinct contract.id from servicedetail inner join contract on contract.id=servicedetail.contract where servicedetail.cName=\'"+name+"\'");
 		
-		# cids=cursor.fetchall();
-		# for row in cids:
-		# 	if row[0] is not None:
-		# 		print name;
-		# 		cid=row[0];
-		# 		#cursor.execute(updateSQL,(originalSchool,originalMajor,gpa,toefl,sat,gre,gmat,cid));
+		cids=cursor.fetchall();
+		for row in cids:
+			if row[0] is not None:
+				print name;
+				cid=row[0];
+				cursor.execute(updateSQL,(originalSchool,originalMajor,gpa,toefl,sat,gre,gmat,cid));
 		# 	else:
 		# 		print line;
 				
